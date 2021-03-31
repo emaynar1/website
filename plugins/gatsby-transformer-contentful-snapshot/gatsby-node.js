2497105c02a328da05d3bb41296ed5846b74459c
Kevin Miller <keveemiller@gmail.com>
2021-03-31 11:38:38 -0700

    chore: CHange to batch after postbuild

--- plugins/gatsby-transformer-contentful-snapshot/gatsby-node.js
+++ plugins/gatsby-transformer-contentful-snapshot/gatsby-node.js
@@ -7,32 +7,62 @@ const client = contentful.createClient({
 
 const latestDate = DateTime.fromISO(process.env.CONTENTFUL_SNAPSHOT_DATE)
 
-const onCreateNode = async ({ node }, configOptions) => {
-  if (node.internal.type !== 'ContentfulPage') {
+const nodeList = []
+
+exports.onCreateNode = async ({ node }, configOptions) => {
+  if (typeof node.contentful_id === 'undefined') {
     return
   }
-  const space = await client.getSpace(process.env.CONTENTFUL_SPACE)
-  const environment = await space.getEnvironment('master')
-  const entry = await environment.getEntry(node.contentful_id)
-  const snapshots = await entry.getSnapshots()
-  let latestSnapshot = false
-  snapshots.forEach(snapshot => {
-    if (
-      !latestSnapshot ||
-      (DateTime.fromISO(snapshot.sys.updatedAt) < latestDate &&
-        DateTime.fromISO(snapshot.sys.updatedAt) >
-          DateTime.fromISO(latestSnapshot.sys.updatedAt))
-    ) {
-      latestSnapshot = snapshot
-    }
+  nodeList.push({
+    id: node.id,
+    contentful_id: node.contentful_id,
   })
-  if (latestSnapshot) {
-    const entrySnapshot = entry.getSnapshot(latestSnapshot.sys.id)
-    console.log(entrySnapshot)
-    process.exit(1)
-  }
-
-  return null
 }
 
-exports.onCreateNode = onCreateNode
+exports.onPostBootstrap = async ({ actions, reporter }) => {
+  const { createNode, deleteNode } = actions
+  reporter.info(
+    `Patching ${
+      nodeList.length
+    } contentful nodes match content from ${latestDate.toISODate()}`,
+  )
+  const space = await client.getSpace(process.env.CONTENTFUL_SPACE)
+  const environment = await space.getEnvironment('master')
+
+  let i = 0
+  const updateSnapshot = async () => {
+    let latestSnapshot = false
+    if (typeof nodeList[i] === 'undefined') {
+      return
+    }
+    const entry = await environment.getEntry(nodeList[i].contentful_id)
+    const snapshots = await entry.getSnapshots()
+    if (!snapshots.items) {
+      reporter.info(`Found no snapshots for ${nodeList[i].contentful_id}`)
+      i += 1
+      updateSnapshot()
+      return
+    }
+    snapshots.items.forEach(snapshot => {
+      if (
+        !latestSnapshot ||
+        (DateTime.fromISO(snapshot.sys.updatedAt) < latestDate &&
+          DateTime.fromISO(snapshot.sys.updatedAt) >
+            DateTime.fromISO(latestSnapshot.sys.updatedAt))
+      ) {
+        latestSnapshot = snapshot
+      }
+    })
+    if (latestSnapshot) {
+      const entrySnapshot = entry.getSnapshot(latestSnapshot.sys.id)
+      reporter.info(`Found snapshot for ${nodeList[i].contentful_id}`)
+      i += 1
+      updateSnapshot()
+    } else {
+      deleteNode({ id: nodeList[i].id })
+      i += 1
+      updateSnapshot()
+    }
+  }
+  updateSnapshot()
+}
